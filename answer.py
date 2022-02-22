8c8d0cd85b01892176d4d70ca8e4cbf78cb6ad63
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2022-02-21 23:54:00 -0500

    Add files via upload

---
+++ answer.py
@@ -0,0 +1,20 @@
+ #!/usr/bin/python
+"""
+Python  Functions
+"""
+
+# Write a function unique_list() that takes a list and returns a new list with unique elements of the first list.
+# unique_list: (1,2,3,3,3,3,4,4,5)
+
+
+
+#Write a function multiply() to multiply all giving numbers in a list.
+#Sample List : (9, 2, 3, -6, 7)
+#Expected Output : -2268
+
+
+
+# Write a function get_average() that takes an arbitrary number of arguments and returns the average of them
+# Number of arguments : (5,6,8,10,31)
+# Expected Output : 12
+
